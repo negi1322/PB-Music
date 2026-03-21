e3092e32043676b41c6cfe97e6650ca675cfdc04
pardeepsinghnegi <pardeepnegi7658@gmail.com>
2026-03-21 19:08:37 +0530

    first commit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -8,6 +8,11 @@ import Loader from "../components/Common/Loader";
 import { AnimatePresence, motion } from "framer-motion";
 import { useLocation } from "react-router-dom";
 import SmallMusic from "../components/Common/SmallMusic";
+import {
+  add_user_favourite,
+  get_user_favourite,
+  remove_user_favourite,
+} from "../api/UserAction";
 
 const API = import.meta.env.VITE_APP_URL;
 
@@ -19,8 +24,27 @@ function Search() {
   const [showLoader, setshowLoader] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
   const [showSmallMusic, setShowSmallMusic] = useState(false);
+  const [favourite, setFavourite] = useState([]);
 
   const location = useLocation();
+  const userData = JSON.parse(localStorage.getItem("user"));
+
+  useEffect(() => {
+    getFavourite();
+  }, []);
+
+  const getFavourite = async () => {
+    try {
+      const data = await get_user_favourite();
+      const videoIds = data?.data?.videoIds;
+
+      if (videoIds && videoIds.length > 0) {
+        setFavourite(videoIds);
+      }
+    } catch (err) {
+      console.error("Error fetching favourites:", err);
+    }
+  };
 
   useEffect(() => {
     if (location.state?.query) {
@@ -64,7 +88,21 @@ function Search() {
       videoID: song?.videoId,
     });
   };
-
+  const handleFavorites = async (song) => {
+    if (favourite?.includes(song?.videoId)) {
+      await remove_user_favourite({
+        email: userData?.email,
+        videoId: song?.videoId,
+      });
+    } else {
+      await add_user_favourite({
+        email: userData?.email,
+        videoId: song?.videoId,
+      });
+    }
+    const updatedFav = await get_user_favourite();
+    setFavourite(updatedFav?.data?.videoIds);
+  };
   return (
     <>
       <AnimatePresence>
@@ -131,14 +169,16 @@ function Search() {
               <div className="row gap-3 flex-column">
                 {songs?.map((song, key) => (
                   <div
-                    onClick={() => {
-                      playSong(song);
-                      setShowSmallMusic(false);
-                    }}
                     key={key}
-                    className="col-12 d-flex flex-row justify-content-between align-items-center pointer play-song"
+                    className="col-12 d-flex flex-row justify-content-between align-items-center  play-song"
                   >
-                    <div className="d-flex flex-row gap-4">
+                    <div
+                      onClick={() => {
+                        playSong(song);
+                        setShowSmallMusic(false);
+                      }}
+                      className="pointer d-flex flex-row gap-4"
+                    >
                       <div>
                         <img
                           src={song?.thumbnails?.[0]?.url}
@@ -157,8 +197,22 @@ function Search() {
                         </p>
                       </div>
                     </div>
-                    <div className="text-secondary fs-6 fw-medium mb-0">
-                      {formatTime(song?.duration)}
+                    <div className="d-flex align-content-center gap-4">
+                      <div className="text-secondary fs-6 fw-medium">
+                        {formatTime(song?.duration)}
+                      </div>
+                      <span
+                        onClick={() => handleFavorites(song)}
+                        className="pointer"
+                      >
+                        <i
+                          className={
+                            favourite?.includes(song?.videoId)
+                              ? "bi bi-heart-fill text-danger fs-5 fw-medium"
+                              : "bi bi-heart text-white fs-5 fw-medium"
+                          }
+                        ></i>
+                      </span>
                     </div>
                   </div>
                 ))}
